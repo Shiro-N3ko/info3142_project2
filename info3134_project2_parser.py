fc9b72a2b547cc8561ed2afa5717fa9930402d10
SaeedSab101 <105342474+SaeedSab101@users.noreply.github.com>
2023-11-29 21:37:55 -0500

    Update info3134_project2_parser.py

--- info3134_project2_parser.py
+++ info3134_project2_parser.py
@@ -4,52 +4,74 @@ import spacy
 nlp = spacy.load("en_core_web_sm")
 
 # Read the EmailLog.txt file
-#with open("EmailLog.txt", "r") as file:
-with open("info3142_project2\EmailLog.txt", "r") as file:
+with open("EmailLog.txt", "r") as file:
     email_log = file.read()
 
 # Split the log entries based on "<<End>>" tag
 log_entries = email_log.split("<<End>>")
 
 # Process each log entry
-output_data = []
+output_data = {}
 
 for entry in log_entries:
     doc = nlp(entry)
 
-    #check if the doc is not empty
-    if len(doc) > 0: 
+    # check if the doc is not empty
+    if len(doc) > 0:
         # Extract email address
-        #email_address = doc[0].text.strip()
-
-        # Extract entities and amounts
         emailAddress = None
-        entities = set() #K:Changed from [] to a set()
-        amounts = set() #K:Changed from [] to a set()
+        entities = set()
+        amounts = []
 
         for token in doc:
             # check if the email is there
             if token.like_email:
                 emailAddress = token.text
 
+        current_amount = 0
         for ent in doc.ents:
             if ent.label_ == "ORG" or ent.label_ == "PRODUCT":
-                entities.add(ent.text) #K: Chnaged from append to Add so that a batch of tokens is added at once 
+                entities.add(ent.text)
             elif ent.label_ == "MONEY":
-                amounts.add(ent.text)
+                # Handle the case where the amount is given in words
+                amount_text = ent.text.replace("$", "").replace(",", "")
+                if amount_text.isdigit():
+                    amounts.append(float(amount_text))
+                else:
+                    # Convert words to numbers
+                    amount_in_words = {'thousand': 1000, 'million': 1000000}
+                    words = amount_text.split()
+                    total_amount = 0
+                    current_amount = 0
+                    for word in words:
+                        if word.isdigit():
+                            current_amount = float(word)
+                        elif word in amount_in_words:
+                            current_amount *= amount_in_words[word]
+                            total_amount += current_amount
+                            current_amount = 0
+                    if current_amount != 0:
+                        total_amount += current_amount
+                        current_amount = 0
+                    amounts.append(total_amount)
 
-        #fromat the amount as currency
-        formatAmounts = "$" + " and ".join(amounts) if amounts else "" 
+        # format the amount as currency
+        formatAmounts = " and ".join(["${:,.2f}".format(amount) for amount in amounts]) if amounts else ""
         formattedEntities = ", ".join(entities)
 
-        #get the total of requests
-
         # Combine entities and amounts for output
-        output_line = f"{emailAddress} : {formatAmounts} and {formattedEntities} " #{' and '.join(amounts)} to {' and '.join(entities)}"
-        output_data.append(output_line)
-
+        if emailAddress:
+            if emailAddress not in output_data:
+                output_data[emailAddress] = {"amounts": [], "entities": set()}
+            
+            output_data[emailAddress]["amounts"].extend(amounts)
+            output_data[emailAddress]["entities"].update(entities)
 
 # Write the output to a text file
 with open("output_data.txt", "w") as output_file:
-    for line in output_data:
-        output_file.write(line + "\n")
+    for email, data in output_data.items():
+        total_amount = sum(data["amounts"])
+        formattedAmount = "${:,.2f}".format(total_amount)
+        formattedEntities = ", ".join(data["entities"])
+        output_line = f"{email} : {formattedAmount} to {formattedEntities}"
+        output_file.write(output_line + "\n")
