8c85d3da20b0e08bc3b06e9f38a601ab4f7eead4
SaeedSab101 <105342474+SaeedSab101@users.noreply.github.com>
2023-11-29 21:37:35 -0500

    Update info3134_project2_output.py

--- info3134_project2_output.py
+++ info3134_project2_output.py
@@ -1,26 +1,15 @@
+# project2_output.py
+
 # Read the stored text file
-with open("TaggedEmailLog.txt", "r") as output_file:
-    output_data = output_file.read().split("<<End>>")
+with open("output_data.txt", "r") as output_file:
+    output_data = output_file.read().split("\n")
 
 # Process and print the console output
 total_amount = 0
 
 for entry in output_data:
-    lines = entry.strip().split('\n')
-    if len(lines) < 2:
-        continue
-    
-    email = lines[0].strip()
-    print(email)
-    
-    # Extract amounts
-    amounts = [int(amount.replace("$", "").replace(",", "")) for amount in lines[1].split() if amount[0] == "$"]
-    total_amount += sum(amounts)
-    
-    # Print the email content
-    content = '\n'.join(lines[2:])
-    print(content)
+    if entry:
+        print(entry)
+        total_amount += float(entry.split(":")[1].split()[0].replace("$", "").replace(",", ""))
 
-    print("<<End>>\n")
-
-print(f"Total Requests: ${'{:,.2f}'.format(total_amount)}")
+print(f"\nTotal Requests: ${'{:,.2f}'.format(total_amount)}")
